b1f84bac0d099a277f3e5845160906cae6503055
Lara Sanders <larasanders@gmail.com>
2017-11-21 00:56:48 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT title FROM projects
-JOIN pledges ON project.id = pledges.project_id
+JOIN pledges ON projects.id = pledges.project_id
 ORDER BY title;"
 end
 
