537d9038697501a78aab32c58eaae8c56aa0ea7e
Lara Sanders <larasanders@gmail.com>
2017-11-21 21:29:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,8 +11,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT title, amount FROM projects
-INNER JOIN projects ON pledges.project_id = projects.id
-ORDER BY title;"
+INNER JOIN pledges ON pledges.project_id = projects.id;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
